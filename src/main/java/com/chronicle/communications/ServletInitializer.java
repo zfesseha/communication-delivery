b5cfb61dc39627be7cce0424268623468feb5feb
ZeleAlem Fesseha <zfesseha1@gmail.com>
2020-04-14 14:28:38 -0400

    Initial Commit. Empty SpringBoot Project.

---
+++ src/main/java/com/chronicle/communications/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.chronicle.communications;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommunicationsApplication.class);
+	}
+
+}
